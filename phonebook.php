05590340e4e2dd7faad1dead7e5bca58eeb71b03
bekasovme <bekasovme@gmail.com>
2018-04-10 22:58:28 +0300

    v.3

--- phonebook.php
+++ phonebook.php
@@ -13,29 +13,65 @@ $data = json_decode($phonebook, true);
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="utf-8">
-    <title>Телефонная книга</title>
+<meta charset="utf-8">
+<title>Телефонная книга</title>
+<style>
+  table {
+  border-spacing: 0 10px;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: bold;
+  }
+  th {
+      padding: 10px 20px;
+      background: #56433D;
+      color: #F9C941;
+      border-right: 2px solid;
+      font-size: 0.9em;
+  }
+  th:first-child {
+      text-align: left;
+  }
+  th:last-child {
+      border-right: none;
+  }
+  td {
+      vertical-align: middle;
+      padding: 10px;
+      font-size: 14px;
+      text-align: center;
+      border-top: 2px solid #56433D;
+      border-bottom: 2px solid #56433D;
+      border-right: 2px solid #56433D;
+  }
+  td:first-child {
+      border-left: 2px solid #56433D;
+      border-right: none;
+  }
+  td:nth-child(2){
+      text-align: left;
+  }
+</style>
 </head>
 <body>
 <table>
-    <thead>
-        <th>
-            <td>Имя</td>
-            <td>Фамилия</td>
-            <td>Адрес</td>
-            <td>Телефон</td>
-        </th>
-    </thead>
-    <tbody>
-    <?php foreach ($data as $item): ?>
-        <tr>
-            <td><?php echo $item['firstName']; ?></td>
-            <td><?php echo $item['lastName']; ?></td>
-            <td><?php echo $item['address']; ?></td>
-            <td><?php echo $item['phoneNumber']; ?></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
+  <thead>
+    <tr>
+      <th>Имя</th>
+      <th>Фамилия</th>
+      <th>Адрес</th>
+      <th>Телефон</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($data as $item): ?>
+    <tr>
+      <td><?php echo $item['firstName']; ?></td>
+      <td><?php echo $item['lastName']; ?></td>
+      <td><?php echo $item['address']; ?></td>
+      <td><?php echo $item['phoneNumber']; ?></td>
+    </tr>
+  <?php endforeach; ?>
+  </tbody>
 </table>
 </body>
 </html>
